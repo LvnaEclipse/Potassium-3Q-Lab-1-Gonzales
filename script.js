e06ee7fa2627a0979ab3a0589ba5400016158fad
LvnaEclipse <izdgonzales29@src.pshs.edu.ph>
2026-02-09 09:35:38 +0800

    script.js

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function solveForTimeForBodyToOrbit() {
     let radius = parseFloat(document.getElementById("radius").value);
     let mass = parseFloat(document.getElementById("mass").value);
 
-    let result = Math.sqrt ((4*Math.pow(PI,2)*Math.pow(radius,3))/(G*mass));ss
+    let result = Math.sqrt ((4*Math.pow(PI,2)*Math.pow(radius,3))/(G*mass));
     let output = `The time it would take for a celestial body to orbit around a bigger celestial body is ${result}.`;
     
     document.getElementById("result").innerText = output;
+
 }
